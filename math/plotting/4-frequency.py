1f0d49193400b8695ad1d2d31711806afc1b5668
imranmmd <mr.imranmammadov@gmail.com>
2026-01-11 15:14:42 +0000

    Task #4 completed

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -19,12 +19,14 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    # Create bins from 0 to 100 in steps of 10
-    bins = np.arange(0, 110, 10)  # 0, 10, 20, ..., 100
+    # your code here
+    bins = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-    # Plot histogram
-    plt.hist(student_grades, bins=bins, edgecolor='black')
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
+    plt.xlabel('Grades')
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    plt.hist(student_grades, bins, edgecolor='black')
+    plt.xticks(np.arange(0, 110, 10))
     plt.show()
