907ce1239a1875668cd48c7642ef6d653a7f698d
imranmmd <mr.imranmammadov@gmail.com>
2026-01-11 14:49:31 +0000

    Task #0 completed

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python3
-"""
-Module for plotting a cubic line graph.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
-    """
-    Plots a cubic line graph where y = x^3 for x values from 0 to 10.
-    """
+    """Plots y as a solid red line graph."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
-    x = np.arange(0, 11)
-
-    plt.plot(x, y, 'r')
+    plt.plot(y, 'r')
+    plt.xlim(0, 10)
     plt.show()
