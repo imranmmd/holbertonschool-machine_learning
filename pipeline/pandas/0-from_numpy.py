85803a18231e9fa0120d006cb6ee1b467e142373
imranmmd <mr.imranmammadov@gmail.com>
2026-01-11 07:45:44 +0000

    Task #0 completed

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-
 import pandas as pd
 
 def from_numpy(arr):
-    if not isinstance(arr, np.ndarray):
-        raise TypeError("Input must be a NumPy array")
-
-    if arr.ndim != 2:
-        raise ValueError("Input array must be 2D")
-
     num_cols = arr.shape[1]
     if num_cols > 26:
         raise ValueError("Array has more than 26 columns")
 
     col_names = [chr(65 + i) for i in range(num_cols)]
-    df = pd.DataFrame(arr, columns=col_names)
-
-    return df
+    return pd.DataFrame(arr, columns=col_names)
