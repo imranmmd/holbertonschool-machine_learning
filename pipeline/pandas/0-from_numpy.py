783aa31ec41971571476cbd1279c3ed7e3ee386a
imranmmd <mr.imranmammadov@gmail.com>
2026-01-11 07:48:42 +0000

    Task #0 completed

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import pandas as pd
+"""Convert a 2D NumPy array to a Pandas DataFrame with column names as uppercase letters."""
 
 
 def from_numpy(arr):
+    """Convert a 2D NumPy array to a Pandas DataFrame with column names as uppercase letters."""
     num_cols = arr.shape[1]
     if num_cols > 26:
         raise ValueError("Array has more than 26 columns")
