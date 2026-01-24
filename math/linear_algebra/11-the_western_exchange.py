7fe8b0512bc7e56e7af849ec0854fe20d4be3394
imranmmd <mr.imranmammadov@gmail.com>
2026-01-24 16:40:51 +0000

    Task #11 completed

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that contains a function to get the shape of a numpy ndarray.
+"""
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy ndarray.
+
+    Args:
+        matrix (numpy.ndarray): The input array.
+
+    Returns:
+        tuple: A tuple of integers representing the shape of the array.
+    """
+    return matrix.T
