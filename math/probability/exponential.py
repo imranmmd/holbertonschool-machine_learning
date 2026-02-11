e0ecf32d069a4b4e6d13003bace384c22310c10e
imranmmd <mr.imranmammadov@gmail.com>
2026-02-11 18:28:05 +0000

    Task #5 completed

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -30,5 +30,14 @@ class Exponential:
             return 0
 
         e = 2.7182818285
-
         return self.lambtha * (e ** (-self.lambtha * x))
+
+    def cdf(self, x):
+        """
+        Calculates the CDF for a given time period x
+        """
+        if x < 0:
+            return 0
+
+        e = 2.7182818285
+        return 1 - (e ** (-self.lambtha * x))
