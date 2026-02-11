20a24f865b7d6d35c86052c55185e00e5497b6b3
imranmmd <mr.imranmammadov@gmail.com>
2026-02-11 18:30:37 +0000

    Task #7 completed

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -29,3 +29,15 @@ class Normal:
             # Calculate population standard deviation
             variance = sum((x - self.mean) ** 2 for x in data) / len(data)
             self.stddev = float(variance ** 0.5)
+
+    def z_score(self, x):
+        """
+        Calculates the z-score of a given x-value
+        """
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """
+        Calculates the x-value of a given z-score
+        """
+        return self.mean + z * self.stddev
