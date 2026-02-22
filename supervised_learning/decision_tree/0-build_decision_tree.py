aae6590df3e4a8ef222a64b18e81506b760853d3
imranmmd <mr.imranmammadov@gmail.com>
2026-02-22 19:49:27 +0000

    Task #0 completed

---
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -0,0 +1,19 @@
+    def max_depth_below(self):
+        """
+        Returns the maximum depth below the current node
+        (including itself and all descendants)
+        """
+        # If both children are None (safety case)
+        if self.left_child is None and self.right_child is None:
+            return self.depth
+
+        left_depth = self.depth
+        right_depth = self.depth
+
+        if self.left_child is not None:
+            left_depth = self.left_child.max_depth_below()
+
+        if self.right_child is not None:
+            right_depth = self.right_child.max_depth_below()
+
+        return max(left_depth, right_depth)
