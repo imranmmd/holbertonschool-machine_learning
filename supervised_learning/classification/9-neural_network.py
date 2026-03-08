afadf300b903a9284aaa4877f7f9587132345e4f
imranmmd <mr.imranmammadov@gmail.com>
2026-03-08 07:13:46 +0000

    task [9]

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""Module that defines a neural network with one hidden layer."""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """Defines a neural network with one hidden layer."""
+
+    def __init__(self, nx, nodes):
+        """Initialize the neural network."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Get the weights of the hidden layer."""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Get the bias of the hidden layer."""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Get the activated output of the hidden layer."""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Get the weights of the output neuron."""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Get the bias of the output neuron."""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Get the activated output of the output neuron."""
+        return self.__A2
