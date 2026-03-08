7c15eb0e0acf21f6e6f9f1aa7551ac17c6c53fc5
imranmmd <mr.imranmammadov@gmail.com>
2026-03-08 07:00:02 +0000

    task 2

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Module that defines a single neuron for binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification."""
+
+    def __init__(self, nx):
+        """Initialize the neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Get the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Get the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Get the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculate the forward propagation of the neuron."""
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
