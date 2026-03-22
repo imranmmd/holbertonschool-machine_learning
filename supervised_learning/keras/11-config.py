d734cf2a291e803246e6a75a47dbd558831b5b78
imranmmd <mr.imranmammadov@gmail.com>
2026-03-22 14:59:47 +0000

    task [11]

---
+++ supervised_learning/keras/11-config.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Saves and loads a model's configuration in JSON format"""
+
+import tensorflow.keras as K
+
+
+def save_config(network, filename):
+    """Saves a model's configuration in JSON format"""
+    with open(filename, 'w') as f:
+        f.write(network.to_json())
+
+
+def load_config(filename):
+    """Loads a model with a specific configuration"""
+    with open(filename, 'r') as f:
+        config = f.read()
+    return K.models.model_from_json(config)
