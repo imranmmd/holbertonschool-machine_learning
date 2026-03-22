84ab3fd873d6bbe736be2c34b640e01ae11acbb9
imranmmd <mr.imranmammadov@gmail.com>
2026-03-22 14:53:51 +0000

    task [7]

---
+++ supervised_learning/keras/7-train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent"""
+    callbacks = []
+
+    if validation_data is not None:
+        if early_stopping:
+            early_stop = K.callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=patience
+            )
+            callbacks.append(early_stop)
+
+        if learning_rate_decay:
+            def lr_decay(epoch):
+                return alpha / (1 + decay_rate * epoch)
+
+            lr_scheduler = K.callbacks.LearningRateScheduler(
+                lr_decay,
+                verbose=1
+            )
+            callbacks.append(lr_scheduler)
+
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data,
+                       callbacks=callbacks)
