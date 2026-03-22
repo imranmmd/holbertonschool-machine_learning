50ca404fe4667e411f9f8b9739565b293708fdf6
imranmmd <mr.imranmammadov@gmail.com>
2026-03-22 14:45:43 +0000

    task [5]

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent"""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent"""
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle,
+                       validation_data=validation_data)
