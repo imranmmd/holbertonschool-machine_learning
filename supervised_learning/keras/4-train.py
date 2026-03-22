0de731e59e32e8661a4c36c49da7ab308e9b2850
imranmmd <mr.imranmammadov@gmail.com>
2026-03-22 14:44:18 +0000

    task [4]

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,31 +1,14 @@
 #!/usr/bin/env python3
 """Trains a model using mini-batch gradient descent"""
 
+import tensorflow.keras as K
+
 
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
-    """
-    Trains a model using mini-batch gradient descent
-
-    Args:
-        network: model to train
-        data: input data of shape (m, nx)
-        labels: one-hot labels of shape (m, classes)
-        batch_size: size of each batch
-        epochs: number of epochs
-        verbose: determines if output is printed during training
-        shuffle: determines whether to shuffle data every epoch
-
-    Returns:
-        The History object generated after training
-    """
-    history = network.fit(
-        x=data,
-        y=labels,
-        batch_size=batch_size,
-        epochs=epochs,
-        verbose=verbose,
-        shuffle=shuffle
-    )
-
-    return history
+    """Trains a model using mini-batch gradient descent"""
+    return network.fit(data, labels,
+                       batch_size=batch_size,
+                       epochs=epochs,
+                       verbose=verbose,
+                       shuffle=shuffle)
