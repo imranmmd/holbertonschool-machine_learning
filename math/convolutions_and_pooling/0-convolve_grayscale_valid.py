e8ce3c666d8d633518347e085a9416ed63208f70
imranmmd <mr.imranmammadov@gmail.com>
2026-03-29 15:13:08 +0000

    Task #0 completed

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+"""
+Module for performing valid convolution on grayscale images.
+"""
+
 import numpy as np
 
 
 def convolve_grayscale_valid(images, kernel):
     """
-    Performs a valid convolution on grayscale images
+    Performs a valid convolution on grayscale images.
+
+    Args:
+        images (numpy.ndarray): shape (m, h, w) containing grayscale images
+        kernel (numpy.ndarray): shape (kh, kw) convolution kernel
+
+    Returns:
+        numpy.ndarray: convolved images of shape (m, h - kh + 1, w - kw + 1)
     """
 
     m, h, w = images.shape
