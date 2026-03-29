c4cc518f801c59ae91af1abd705b550ab40f89c7
imranmmd <mr.imranmammadov@gmail.com>
2026-03-29 15:54:28 +0000

    Task #13 completed

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -8,7 +8,7 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
-    Normalizes an unactivated output of a neural network using batch normalization.
+    Normalizes output of a neural network using batch normalization.
 
     Args:
         Z (np.ndarray): shape (m, n), unactivated outputs
