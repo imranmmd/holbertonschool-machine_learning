3c15e1e33557898ddc908202bd52ea3c3d71f58d
imranmmd <mr.imranmammadov@gmail.com>
2026-03-29 15:50:45 +0000

    Task #12 completed

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -17,7 +17,7 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
         decay_step (int): number of steps before decaying further
 
     Returns:
-        tf.keras.optimizers.schedules.LearningRateSchedule: 
+        tf.keras.optimizers.schedules.LearningRateSchedule:
         the learning rate decay operation
     """
     alpha_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
