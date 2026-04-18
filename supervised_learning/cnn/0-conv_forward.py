95208b51fbeb49bda20db9713976d1989c7d5476
imran.mmd <mr.imranmammadov@gmail.com>
2026-04-18 09:19:31 +0000

    Task #0 completed

---
+++ supervised_learning/cnn/0-conv_forward.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
+    """
+    Performs forward propagation over a convolutional layer
+
+    Parameters:
+    A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+    W: numpy.ndarray of shape (kh, kw, c_prev, c_new)
+    b: numpy.ndarray of shape (1, 1, 1, c_new)
+    activation: activation function
+    padding: "same" or "valid"
+    stride: tuple (sh, sw)
+
+    Returns:
+    A: output of the convolutional layer
+    """
+
+    m, h_prev, w_prev, c_prev = A_prev.shape
+    kh, kw, _, c_new = W.shape
+    sh, sw = stride
+
+    # Padding
+    if padding == "same":
+        ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
+        pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
+    else:  # valid
+        ph, pw = 0, 0
+
+    A_prev_pad = np.pad(
+        A_prev,
+        ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+        mode='constant',
+        constant_values=0
+    )
+
+    # Output dimensions
+    h_new = int((h_prev + 2 * ph - kh) / sh) + 1
+    w_new = int((w_prev + 2 * pw - kw) / sw) + 1
+
+    # Initialize output
+    Z = np.zeros((m, h_new, w_new, c_new))
+
+    # Convolution
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    slice_prev = A_prev_pad[i,
+                                             vert_start:vert_end,
+                                             horiz_start:horiz_end,
+                                             :]
+
+                    Z[i, h, w, c] = np.sum(
+                        slice_prev * W[:, :, :, c]
+                    ) + b[:, :, :, c]
+
+    # Apply activation
+    A = activation(Z)
+
+    return A
