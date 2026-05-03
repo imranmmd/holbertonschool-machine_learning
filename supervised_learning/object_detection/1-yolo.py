d9709cadcdc196dba49208703b3362c6d935d5d0
imranmmd <mr.imranmammadov@gmail.com>
2026-05-03 16:20:48 +0000

    task 1

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -9,7 +9,7 @@ class Yolo:
     """Uses YOLO v3 to perform object detection"""
 
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
-        """Initialize YOLO"""
+        """Class constructor"""
         self.model = K.models.load_model(model_path)
 
         with open(classes_path, "r") as f:
@@ -19,18 +19,17 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
-    @staticmethod
-    def sigmoid(x):
-        """Sigmoid activation"""
+    def sigmoid(self, x):
+        """Calculates sigmoid"""
         return 1 / (1 + np.exp(-x))
 
     def process_outputs(self, outputs, image_size):
         """
-        Process Darknet outputs
+        Processes Darknet outputs
 
         Args:
-            outputs: list of numpy arrays
-            image_size: numpy array [image_height, image_width]
+            outputs: list of prediction tensors
+            image_size: numpy.ndarray [image_height, image_width]
 
         Returns:
             boxes, box_confidences, box_class_probs
@@ -42,43 +41,46 @@ class Yolo:
         input_h = self.model.input.shape[1]
         input_w = self.model.input.shape[2]
 
-        img_h = image_size[0]
-        img_w = image_size[1]
+        image_h = image_size[0]
+        image_w = image_size[1]
 
         for i, output in enumerate(outputs):
             grid_h = output.shape[0]
             grid_w = output.shape[1]
-            anchors = self.anchors[i]
 
-            tx = output[..., 0]
-            ty = output[..., 1]
-            tw = output[..., 2]
-            th = output[..., 3]
+            t_x = output[..., 0]
+            t_y = output[..., 1]
+            t_w = output[..., 2]
+            t_h = output[..., 3]
 
-            cx = np.arange(grid_w)
-            cy = np.arange(grid_h)
+            anchor_w = self.anchors[i, :, 0]
+            anchor_h = self.anchors[i, :, 1]
 
-            cx = np.tile(cx, grid_h).reshape(grid_h, grid_w)
-            cy = np.tile(cy.reshape(-1, 1), grid_w)
+            c_x = np.arange(grid_w).reshape(1, grid_w, 1)
+            c_x = np.tile(c_x, (grid_h, 1, output.shape[2]))
 
-            bx = (self.sigmoid(tx) + cx[..., np.newaxis]) / grid_w
-            by = (self.sigmoid(ty) + cy[..., np.newaxis]) / grid_h
+            c_y = np.arange(grid_h).reshape(grid_h, 1, 1)
+            c_y = np.tile(c_y, (1, grid_w, output.shape[2]))
 
-            bw = (anchors[:, 0] * np.exp(tw)) / input_w
-            bh = (anchors[:, 1] * np.exp(th)) / input_h
+            b_x = (self.sigmoid(t_x) + c_x) / grid_w
+            b_y = (self.sigmoid(t_y) + c_y) / grid_h
 
-            x1 = (bx - bw / 2) * img_w
-            y1 = (by - bh / 2) * img_h
-            x2 = (bx + bw / 2) * img_w
-            y2 = (by + bh / 2) * img_h
+            b_w = (anchor_w * np.exp(t_w)) / input_w
+            b_h = (anchor_h * np.exp(t_h)) / input_h
 
-            box = np.stack((x1, y1, x2, y2), axis=-1)
+            x1 = (b_x - b_w / 2) * image_w
+            y1 = (b_y - b_h / 2) * image_h
+            x2 = (b_x + b_w / 2) * image_w
+            y2 = (b_y + b_h / 2) * image_h
+
+            box = np.zeros(output[..., :4].shape)
+            box[..., 0] = x1
+            box[..., 1] = y1
+            box[..., 2] = x2
+            box[..., 3] = y2
             boxes.append(box)
 
-            confidence = self.sigmoid(output[..., 4:5])
-            box_confidences.append(confidence)
-
-            class_probs = self.sigmoid(output[..., 5:])
-            box_class_probs.append(class_probs)
+            box_confidences.append(self.sigmoid(output[..., 4:5]))
+            box_class_probs.append(self.sigmoid(output[..., 5:]))
 
         return boxes, box_confidences, box_class_probs
