ea2e71baddd2537f8a64efd049d6ecb174b3fccd
imranmmd <mr.imranmammadov@gmail.com>
2026-05-31 14:57:05 +0000

    task [0]

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -5,20 +5,10 @@ import numpy as np
 
 
 class GaussianProcess:
-    """Represents a noiseless 1D Gaussian Process"""
+    """Represents a noiseless 1D Gaussian process"""
 
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """
-        Class constructor
-
-        Args:
-            X_init: numpy.ndarray of shape (t, 1)
-                Initial inputs sampled
-            Y_init: numpy.ndarray of shape (t, 1)
-                Initial outputs sampled
-            l: length parameter for the kernel
-            sigma_f: signal standard deviation
-        """
+        """Initialize the Gaussian process"""
         self.X = X_init
         self.Y = Y_init
         self.l = l
@@ -35,14 +25,12 @@ class GaussianProcess:
             X2: numpy.ndarray of shape (n, 1)
 
         Returns:
-            Covariance kernel matrix of shape (m, n)
+            numpy.ndarray of shape (m, n)
         """
-        sqdist = (
-            np.sum(X1 ** 2, axis=1).reshape(-1, 1)
-            + np.sum(X2 ** 2, axis=1)
-            - 2 * np.matmul(X1, X2.T)
-        )
+        sqdist = np.sum(X1 ** 2, axis=1).reshape(-1, 1)
+        sqdist += np.sum(X2 ** 2, axis=1)
+        sqdist -= 2 * np.matmul(X1, X2.T)
 
-        return (self.sigma_f ** 2) * np.exp(
-            -0.5 * sqdist / (self.l ** 2)
-    )
+        return self.sigma_f ** 2 * np.exp(
+            -0.5 * sqdist / self.l ** 2
+        )
