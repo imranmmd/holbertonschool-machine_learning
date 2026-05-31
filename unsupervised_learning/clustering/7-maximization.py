f7df18e4d1803f47f47e20185ce53012da9af8ac
imranmmd <mr.imranmammadov@gmail.com>
2026-05-31 15:54:56 +0000

    task [7]

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -5,7 +5,9 @@ import numpy as np
 
 
 def maximization(X, g):
-    """Performs the maximization step in the EM algorithm for a GMM"""
+    """
+    Performs the maximization step in the EM algorithm for a GMM
+    """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
     if not isinstance(g, np.ndarray) or len(g.shape) != 2:
@@ -16,20 +18,20 @@ def maximization(X, g):
 
     if n2 != n:
         return None, None, None
+    if np.any(g < 0) or np.any(g > 1):
+        return None, None, None
+    if not np.allclose(np.sum(g, axis=0), 1):
+        return None, None, None
 
     Nk = np.sum(g, axis=1)
     if np.any(Nk == 0):
         return None, None, None
 
     pi = Nk / n
+    m = (g @ X) / Nk[:, np.newaxis]
 
-    m = np.zeros((k, d))
-    S = np.zeros((k, d, d))
-
-    for i in range(k):
-        m[i] = np.sum(g[i, :, None] * X, axis=0) / Nk[i]
-
-        diff = X - m[i]
-        S[i] = (g[i, :, None] * diff).T @ diff / Nk[i]
+    diff = X[np.newaxis, :, :] - m[:, np.newaxis, :]
+    S = np.einsum('kn,kni,knj->kij', g, diff, diff)
+    S = S / Nk[:, np.newaxis, np.newaxis]
 
     return pi, m, S
