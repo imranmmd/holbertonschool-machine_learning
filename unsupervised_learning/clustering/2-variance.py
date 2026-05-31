b8a31df8cce0577bf9a972ee2db910b7c26160cc
imranmmd <mr.imranmammadov@gmail.com>
2026-05-31 15:32:16 +0000

    task [2]

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -7,13 +7,6 @@ import numpy as np
 def variance(X, C):
     """
     Calculates total intra-cluster variance
-
-    Args:
-        X: (n, d) data points
-        C: (k, d) centroids
-
-    Returns:
-        total variance or None
     """
     if not isinstance(X, np.ndarray) or not isinstance(C, np.ndarray):
         return None
@@ -21,16 +14,12 @@ def variance(X, C):
         return None
     if X.shape[0] == 0 or C.shape[0] == 0:
         return None
+    if X.shape[1] != C.shape[1]:
+        return None
 
-    # compute distance from each point to each centroid
     dist = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
-
-    # assign each point to closest centroid
     clss = np.argmin(dist, axis=1)
 
-    # compute squared distances to assigned centroid
     closest = C[clss]
 
-    var = np.sum((X - closest) ** 2)
-
-    return var
+    return np.sum((X - closest) ** 2)
