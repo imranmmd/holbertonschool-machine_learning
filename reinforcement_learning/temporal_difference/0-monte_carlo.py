559c0b7204efd3efeed073363462856204977315
imranmmd <mr.imranmammadov@gmail.com>
2026-08-29 17:09:55 +0000

    Task #0 completed

---
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Monte Carlo algorithm."""
+import numpy as np
+
+
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
+                alpha=0.1, gamma=0.99):
+    """Perform the Monte Carlo algorithm."""
+    for _ in range(episodes):
+        state = env.reset()[0]
+        episode = []
+
+        for _ in range(max_steps):
+            action = policy(state)
+            new_state, reward, terminated, truncated, _ = env.step(action)
+
+            episode.append((state, reward))
+            state = new_state
+
+            if terminated or truncated:
+                break
+
+        G = 0
+
+        for state, reward in reversed(episode):
+            G = gamma * G + reward
+            V[state] = V[state] + alpha * (G - V[state])
+
+    return V
