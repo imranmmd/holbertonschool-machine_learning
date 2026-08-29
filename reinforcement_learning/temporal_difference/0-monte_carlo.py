d365fcdab7fe6bbf7eaea3e66a9bda332a0b12d3
imranmmd <mr.imranmammadov@gmail.com>
2026-08-29 17:15:34 +0000

    Task #0 completed

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,35 +1,52 @@
 #!/usr/bin/env python3
-"""Monte Carlo algorithm."""
+"""
+Monte Carlo module for reinforcement learning.
+"""
 import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
                 alpha=0.1, gamma=0.99):
-    """Perform the Monte Carlo algorithm."""
+    """
+    Performs the Monte Carlo algorithm for policy evaluation.
+
+    Args:
+        env: environment instance
+        V: numpy.ndarray of shape (s,) containing the value estimate
+        policy: function that takes in a state and returns next action
+        episodes: total number of episodes to train over
+        max_steps: maximum number of steps per episode
+        alpha: learning rate
+        gamma: discount rate
+
+    Returns:
+        V: updated value estimate
+    """
     for _ in range(episodes):
-        state = env.reset()[0]
+        res = env.reset()
+        state = res[0] if isinstance(res, tuple) else res
         episode = []
 
         for _ in range(max_steps):
             action = policy(state)
-            next_state, reward, terminated, truncated, _ = env.step(action)
+            step_res = env.step(action)
+            if len(step_res) == 5:
+                next_state, reward, terminated, truncated, _ = step_res
+                done = terminated or truncated
+            else:
+                next_state, reward, done, _ = step_res
 
             episode.append((state, reward))
+            if done:
+                break
             state = next_state
 
-            if terminated or truncated:
-                break
-
-        for t in range(len(episode)):
-            state = episode[t][0]
-
-            if state in [s for s, _ in episode[:t]]:
-                continue
-
-            G = 0
-            for i in range(t, len(episode)):
-                G = gamma * G + episode[i][1]
-
-            V[state] = V[state] + alpha * (G - V[state])
+        G = 0
+        states = [x[0] for x in episode]
+        for t in range(len(episode) - 1, -1, -1):
+            s, r = episode[t]
+            G = gamma * G + r
+            if s not in states[:t]:
+                V[s] = V[s] + alpha * (G - V[s])
 
     return V
