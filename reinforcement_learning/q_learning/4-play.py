c881eccf414ce489e1197984fd912f3c04392ca7
imranmmd <mr.imranmammadov@gmail.com>
2026-08-29 17:00:45 +0000

    Task #4 completed

---
+++ reinforcement_learning/q_learning/4-play.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Play an episode using a trained Q-table."""
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """Have the trained agent play one episode."""
+    state = env.reset()[0]
+    total_rewards = 0
+    rendered_outputs = []
+
+    for _ in range(max_steps):
+        action = np.argmax(Q[state])
+
+        new_state, reward, terminated, truncated, info = env.step(action)
+
+        total_rewards += reward
+        rendered_outputs.append(env.render())
+
+        state = new_state
+
+        if terminated or truncated:
+            break
+
+    return total_rewards, rendered_outputs
